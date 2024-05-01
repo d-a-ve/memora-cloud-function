620dedeea9038a15da87daf849d71a091a9d6ac7
Dave <davearonmwan@gmail.com>
2024-05-01 11:00:28 +0100

    finally update package.json

--- env.d.ts
+++ env.d.ts
@@ -1,16 +1,18 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-          APPWRITE_API_KEY: string;
-          APPWRITE_PROJECT_ID: string;
-          APPWRITE_MEMORA_DB_ID: string;
-          APPWRITE_BIRTHDAYS_COL_ID: string;
-          PROJECT_EMAIL_ADDRESS: string;
-          PROJECT_EMAIL_PASSWORD: string;
-          MAILTRAP_TEST_USERNAME: string;
-          MAILTRAP_TEST_PASSWORD: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      APPWRITE_API_KEY: string;
+      APPWRITE_PROJECT_ID: string;
+      APPWRITE_MEMORA_DB_ID: string;
+      APPWRITE_BIRTHDAYS_COL_ID: string;
+      PROJECT_EMAIL_ADDRESS: string;
+      PROJECT_EMAIL_PASSWORD: string;
+      MAILTRAP_TEST_USERNAME: string;
+      MAILTRAP_TEST_PASSWORD: string;
+      COURIER_AUTH_TOKEN: string;
+      COURIER_BIRTHDAY_NOTIFICATION_ID: string;
     }
+  }
 }
 
 export {};
