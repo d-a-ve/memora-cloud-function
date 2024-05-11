e44543409f8c0405c7ccc8224be4b1fb2726396f
Dave <davearonmwan@gmail.com>
2024-05-11 19:48:28 +0100

    add another option to access control allow headers

--- src/cors.ts
+++ src/cors.ts
@@ -15,7 +15,7 @@ export function getCorsHeaders(req: any) {
 
   return {
     "Access-Control-Allow-Headers":
-      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
     "Content-Type": "application/json; charset=utf-8",
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH",
     "Access-Control-Allow-Origin":
