562c509200ec87b2188c6ea5a46a0d3228e71201
Dave <davearonmwan@gmail.com>
2024-05-11 20:02:57 +0100

    pushing

--- src/cors.ts
+++ src/cors.ts
@@ -14,8 +14,8 @@ export function getCorsHeaders(req: any) {
   if (!req.headers["origin"]) return {};
 
   return {
-    "Access-Control-Allow-Headers":
-      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
+    // "Access-Control-Allow-Headers":
+    //   "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
     "Content-Type": "application/json; charset=utf-8",
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PATCH",
     "Access-Control-Allow-Origin":
