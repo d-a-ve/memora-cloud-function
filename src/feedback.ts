4e4dbecd9e4595f96f4557371796b931bb51c6ad
Dave <davearonmwan@gmail.com>
2024-05-11 17:22:52 +0100

    init feedback file

---
+++ src/feedback.ts
@@ -0,0 +1,18 @@
+type Context = {
+  req: any;
+  res: any;
+  log: (msg: any) => void;
+  error: (msg: any) => void;
+};
+
+export default async ({ req, res, log }: Context) => {
+  try {
+    const { body } = req;
+
+    log(body);
+    return res.send("Request went well");
+  } catch (e: any) {
+    log(`ERROR: An error happened, ${e}`);
+    return res.send("An error happened, check the logs for more info");
+  }
+};
