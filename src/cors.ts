15e34bd8ffce0aac295470b29bb3f02b30f78214
Dave <davearonmwan@gmail.com>
2024-05-11 19:30:00 +0100

    comment out content type

--- src/cors.ts
+++ src/cors.ts
@@ -14,7 +14,7 @@ export function getCorsHeaders(req: any) {
   if (!req.headers["origin"]) return {};
   
   return {
-    "Content-Type": "application/json",
+    // "Content-Type": "application/json; charset=utf-8",
     "Access-Control-Allow-Origin":
       !process.env.ALLOWED_ORIGINS || process.env.ALLOWED_ORIGINS === "*"
         ? "*"
